59a061f6b03ad1a3cdb0e756a497d70ac237fa5e
James Vos <almagestfraternite@gmail.com>
2015-07-23 02:43:52 +0200

    Overhaul

--- common/config/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('m2m', dirname(dirname(__DIR__)) . '/m2m');
-Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
--- tests/codeception/config/frontend/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Application configuration for all m2m test types
- */
-return [];
